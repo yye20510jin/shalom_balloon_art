57325b925930cf2019bd634d4704a37450183d30
yye20510jin <yye20510@naver.com>
2025-11-20 17:37:53 +0900

    기본 설정

---
+++ src/main/java/com/shalom/shalom_balloon_art/ShalomBalloonArtApplication.java
@@ -0,0 +1,13 @@
+package com.shalom.shalom_balloon_art;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShalomBalloonArtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShalomBalloonArtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shalom/shalom_balloon_art/ShalomBalloonArtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shalom.shalom_balloon_art;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShalomBalloonArtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
