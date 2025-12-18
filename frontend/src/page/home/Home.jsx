4dec2e266d058dff6a4f302df392eaac2db23949
yye20510jin <yye20510@naver.com>
2025-12-18 22:44:34 +0900

    feat: Home 페이지에 게시글 썸네일 표시

--- frontend/src/page/home/Home.jsx
+++ frontend/src/page/home/Home.jsx
@@ -1,9 +1,54 @@
+import { useEffect, useState } from "react";
 import Navbar from "../../components/common/Navbar"
-function Home(){
-    return(
-        <div style={{padding:"20px"}}>
-        <Navbar />
-        <h1>샬롬 풍선 아트</h1>
+import { authFetch } from "../../api/authFetch";
+function Home() {
+
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            try {
+                const res = await authFetch(`${import.meta.env.VITE_BACKEND_BASE_URL}/api/posts`, {
+                    method: "GET",
+                });
+
+                if (!res) return;
+
+                const data = await res.json();
+                setPosts(data);
+
+            } catch (err) {
+                console.error(err);
+            }
+
+        };
+
+        fetchPosts();
+    },[]);
+
+
+    return (
+        <div style={{ padding: "20px" }}>
+            <Navbar />
+            <h1>샬롬 풍선 아트</h1>
+
+            {posts.map((post) => (
+                post.thumbnailUrl && (
+                    <div key={post.index} style={{ flex: "0 0 120px" }}>
+                        <img
+                            src={post.thumbnailUrl}
+                            alt={post.title}
+                            style={{
+                            width: "120px",
+                            height: "80px",
+                            objectFit: "cover",
+                            borderRadius: 4,
+                            }}
+                        />
+                    </div>
+                )
+            ))}
         </div>
+
     );
-}export default Home;
+} export default Home;
