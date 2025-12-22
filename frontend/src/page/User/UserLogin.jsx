8890f5191fbd084c842311eae42d853b23cb6a82
yye20510jin <yye20510@naver.com>
2025-12-22 17:55:22 +0900

    style:로그인 페이지 css 디자인

--- frontend/src/page/User/UserLogin.jsx
+++ frontend/src/page/User/UserLogin.jsx
@@ -1,32 +1,33 @@
-import {useState,useContext} from "react";
-import {useNavigate} from "react-router-dom";
+import { useState, useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import AuthContext from "../../auth/AuthContext"
+import shalomLogo from "../../assets/shalomBalloonArt.png";
+import "../../styles/user/userLogin.css";
 
-function UserLogin(){
+function UserLogin() {
 
-    const [userId,setUserId] = useState("");
-    const [password,setPassword] = useState("");
+    const [userId, setUserId] = useState("");
+    const [password, setPassword] = useState("");
     const [error, setError] = useState("");
-    const {login} = useContext(AuthContext);
+    const { login } = useContext(AuthContext);
     const navigate = useNavigate();
 
-    const handleSubmit = async (e) =>{
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        try{
+        try {
             const response = await fetch(
                 `${import.meta.env.VITE_BACKEND_BASE_URL}/api/auth/userLogin`,
                 {
-                    method:"POST",
-                    headers:{
-                        "Content-Type" : "application/json"
+                    method: "POST",
+                    headers: {
+                        "Content-Type": "application/json"
                     },
-                    body:JSON.stringify({userId,password}),
+                    body: JSON.stringify({ userId, password }),
                 }
             );
 
-            if(!response.ok){
-                console.log("일치하지 않음.");
+            if (!response.ok) {
                 const err = await response.json();
                 setError(err.error);
                 return;
@@ -34,26 +35,36 @@ function UserLogin(){
 
             const data = await response.json();
 
-            login(data.accessToken,data.userId,JSON.stringify(data.roles));
-            navigate("/",{replace : true});
+            login(data.accessToken, data.userId, JSON.stringify(data.roles));
+            navigate("/", { replace: true });
 
-        }catch(err){
+        } catch (err) {
             console.error(err);
             setError("알 수 없는 에러 발생...");
         }
     };
 
-    return(
-        <div style={{padding:"20px"}}>
-            <h1>회원 로그인</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <input type="text" value={userId} onChange={(e)=>setUserId(e.target.value)} placeholder="아이디"/>
-                    <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="비밀번호"/>
-                </div>
-                {error && <p style={{color:"red"}}>{error}</p>}
-                <button type="submit">로그인</button>
-            </form>
+    useEffect(() => {
+        if (error) {
+            setTimeout(() => {
+                setError("");
+            }, 2000);
+        }
+    }, [error]);
+
+    return (
+        <div className="userLogin">
+            <div className="userLogin-box">
+                <img className="userLogin-logo" src={shalomLogo} alt="shalom" />
+                {error ? <div className="userLogin-err" style={{ color: "red" }}>{error}</div> : <div className="userLogin-err" ></div>}
+                <form onSubmit={handleSubmit}>
+                    <div className="userLogin-form">
+                        <input className="userLogin-input" type="text" value={userId} onChange={(e) => setUserId(e.target.value)} placeholder="아이디" />
+                        <input className="userLogin-input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="비밀번호" />
+                        <button type="submit">로그인</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
-}export default UserLogin;
+} export default UserLogin;
