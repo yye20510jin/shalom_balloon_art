6ad56181287d416dd345fcddd275a8742c6dd9bb
yye20510jin <yye20510@naver.com>
2025-12-26 21:26:58 +0900

    style:관리자 로그인 페이지 css 디자인

--- frontend/src/page/admin/AdminLogin.jsx
+++ frontend/src/page/admin/AdminLogin.jsx
@@ -1,34 +1,36 @@
-import {useNavigate} from "react-router-dom"
-import { useState,useContext } from "react"
+import { useNavigate } from "react-router-dom"
+import { useState, useContext } from "react"
 import AuthContext from "../../auth/AuthContext";
+import shalomLogo from "../../assets/shalomBalloonArt.png";
+import "../../styles/admin/adminLogin.css";
 
 
-function AdminLogin(){
+function AdminLogin() {
 
-    const[userId,setUserId] = useState("");
-    const[password, setPassword] = useState("");
-    const[error, setError] = useState("");
-    const { login } = useContext(AuthContext); 
+    const [userId, setUserId] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState("");
+    const { login } = useContext(AuthContext);
 
     const navigate = useNavigate();
 
     //form 기본 submit 막기
-    const handleSubmit = async (e) =>{
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        
-        try{
+
+        try {
             const response = await fetch(
                 `${import.meta.env.VITE_BACKEND_BASE_URL}/api/auth/adminLogin`,
                 {
                     method: "POST",
-                    headers:{
-                        "Content-Type":"application/json",
+                    headers: {
+                        "Content-Type": "application/json",
                     },
-                    body : JSON.stringify({userId, password}),
+                    body: JSON.stringify({ userId, password }),
                 }
             );
 
-            if(!response.ok){
+            if (!response.ok) {
                 const err = await response.json();
                 setError(err.error);
                 return;
@@ -37,27 +39,29 @@ function AdminLogin(){
             const data = await response.json();
 
             //서버가 보낸 토큰 저장
-            login(data.accessToken,data.userId,data.roles);
-            navigate("/admin",{replace : true});
+            login(data.accessToken, data.userId, data.roles);
+            navigate("/admin", { replace: true });
 
-        }catch(err){
+        } catch (err) {
             console.error(err);
             setError("알 수 없는 에러 발생..");
         }
     }
 
-    return(
-        <div style={{padding:20}}>
-            <h1>로그인</h1>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <input type="text" value={userId} onChange={(e)=>setUserId(e.target.value)} placeholder="아이디"/>
-                    <input type="password" value={password} onChange={(e)=>setPassword(e.target.value)} placeholder="비밀번호"/>
-                </div>
-                {error && <p style={{color:"red"}}>{error}</p>}
-                <button type="submit">로그인</button>
-            </form>
+    return (
+        <div className="adminLogin">
+            <div className="adminLogin-box">
+                <img className="adminLogin-logo" src={shalomLogo} alt="shalom" />
+                {error ? <div className="adminLogin-err" style={{ color: "red" }}>{error}</div> : <div className="userLogin-err" ></div>}
+                <form onSubmit={handleSubmit}>
+                    <div className="adminLogin-form">
+                        <input className="adminLogin-input" type="text" value={userId} onChange={(e) => setUserId(e.target.value)} placeholder="아이디" />
+                        <input className="adminLogin-input" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="비밀번호" />
+                        <button type="submit">로그인</button>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 
-}export default AdminLogin;
+} export default AdminLogin;
