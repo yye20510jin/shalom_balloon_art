5480b1afe63705852ac2c44a53d30fbe76628a86
yye20510jin <yye20510@naver.com>
2026-03-19 20:50:26 +0900

    build: local DB를 MySQL에서 PostgreSQL로 변경

--- src/main/java/com/shalom/shalom_balloon_art/config/FirebaseConfig.java
+++ src/main/java/com/shalom/shalom_balloon_art/config/FirebaseConfig.java
@@ -15,7 +15,7 @@ import java.nio.charset.StandardCharsets;
 @Configuration
 public class FirebaseConfig {
 
-   @Value("${firebase.config}")
+   @Value("${firebase.config:}")
    private String firebaseConfig;
 
     @Bean
